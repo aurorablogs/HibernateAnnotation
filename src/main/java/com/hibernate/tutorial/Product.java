9c267b6b107305c966ea16001f78e89ed34cf290
bilalsha <m.bilalshah1@gmail.com>
2014-03-08 23:39:43 +0500

    Hibernate application using Annotation

---
+++ src/main/java/com/hibernate/tutorial/Product.java
@@ -0,0 +1,72 @@
+package com.hibernate.tutorial;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+/**
+ * Model class for Product
+ */
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    /**
+	 * Product Class using Annotation
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+    private String code;
+    private String name;
+    private BigDecimal price;
+    
+    @Id
+    @GenericGenerator(name="generator", strategy="increment")
+	@GeneratedValue(generator="generator")
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "code", unique = true, nullable = false, length = 10)
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	@Column(name = "name", unique = true, nullable = false, length = 10)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "price", unique = true, nullable = false, length = 10)
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	@Override
+    public String toString() {
+        return "Product [id=" + id + ", code=" + code + ", name="
+                + name + ", price=" + price + "]";
+    } 
+    
+}
